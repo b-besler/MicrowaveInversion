4813e0bad8cd62b10a3ab8b5d0abba4c619fcad0
Brendon Besler <brendon.besler@ucalgary.ca>
2021-03-01 15:41:44 -0700

    Change comments and variable names

--- mwi/cli/inverse.py
+++ mwi/cli/inverse.py
@@ -1,14 +1,11 @@
 import argparse
 from mwi.util import read_config
 
-def inverse(model_config, prior_config, meas_config):
-    print(model_config)
-    print(prior_config)
-    print(meas_config)
-    meas_data = read_config.read_meas_config(meas_config)
-    model_data = read_config.read_model_config(model_config)
-    print(meas_data)
-    print(model_data)
+def inverse(model_config_file, prior_config_file, meas_config_file):
+    # Read in .json configuration data
+    meas_data = read_config.read_meas_config(meas_config_file)
+    model_data = read_config.read_model_config(model_config_file)
+    prior_data = read_config.read_model.config(prior_config_file)
 
 def main():
     description ='''Microwave Inverse
@@ -16,7 +13,15 @@ def main():
     Example usage:
     inverse /models/homogeneous_ellipse/bkgd.json /models/homogeneous_ellipse/ellipse.json /models/homogeneous_ellipse/meas.json
 
-
+    Calls the inverse function which:
+        - read in configuration files
+        - sets up gprMax forward solver files
+        - calls gprMax forward solver
+        - applies inverse algorithms
+    
+    #TODO
+    Inverse solve parameters can be varied to include: 
+        --born_type (born, iter, distort) - Born approximation, Iterative Born Method, and Distorted Iterative Born Method
     '''
     # Setup argument parsing
     parser = argparse.ArgumentParser(
@@ -24,9 +29,9 @@ def main():
         prog="inverse",
         description=description
     )
-    parser.add_argument('model_config', help='.json file with model configuration')
-    parser.add_argument('prior_config', help='.json file with a priori model configuration')
-    parser.add_argument('meas_config', help='.json file with measurement setup configuration')
+    parser.add_argument('model_config_file', help='.json file with model configuration')
+    parser.add_argument('prior_config_file', help='.json file with a priori model configuration')
+    parser.add_argument('meas_config_file', help='.json file with measurement setup configuration')
 
     # Parse args and display
     args = parser.parse_args()
