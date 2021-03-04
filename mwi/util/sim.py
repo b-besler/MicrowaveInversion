e834876b4bdf948092dba847713cdbda4a09a157
Brendon Besler <brendon.besler@ucalgary.ca>
2021-03-04 16:21:27 -0700

    Add make sim

--- mwi/util/sim.py
+++ mwi/util/sim.py
@@ -1,9 +1,12 @@
 import numpy as np
+import os
+import pathlib
+
+# functions for gprMax simulation
 
 
 class Source():
-    """Source class gpr max sim
-    """
+    """Class for gpr max simulation source"""
 
     def __init__(self, source_config):
         """Initialize source using config settings
@@ -16,4 +19,66 @@ class Source():
         self.f0 = source_config["f0"]
         self.amp = source_config["amp"]
 
+def make(model, src, folder):
+    """Create gprMax simulation files for model object. Consists of *in text files with gprMax commands and geometry files
+    Args:
+        - model (class): class with rx/tx locations, er/sig images, and image domain information
+        - src (class): class holding source information
+        - folder (string): output folder to make simulation files in
+    """
+    
+    folderPath = os.path.join(folder,model.name)
+    # create new folder with model name
+    if not os.path.exists(folderPath):
+        pathlib.Path(folderPath).mkdir(parents=True,exist_ok=True)
         
+    geom = False
+    # if not just free space we have to make a geometry file
+    if (np.any(model.er-1 > 0.00001) and np.any(model.sig-0 < 0.00001)):
+            #sim.make_geometry(model)
+            geom = True
+        
+    #calculate rx/tx locations
+    tx = model.rx.calc_tx_discrete(model.dx,model.dy)
+    rx = model.rx.calc_rx_discrete(model.dx,model.dy)
+
+    #different simulation file (*.in) for each tx
+    for i in range(model.ntx): 
+        # write gprMax simulation file (see example/gpr_max_sim) and gprMax docs for details.
+        fileName = os.path.join(folderPath,model.name + '_Tx' + str(i)+".in")
+            
+        f = open(fileName,"w")
+            
+        f.write("#title: %s\n" % model.name) #write title
+        f.write("#domain: %f %f %f\n" % (model.x_size, model.y_size, model.dx)) #domain extent command
+        f.write("#dx_dy_dz: %f %f %f\n" % (model.dx, model.dx, model.dx)) #discretization command
+        f.write("#time_window: %e\n" % src.t0) #time window command, length t0
+        f.write("#waveform: %s %f %e my_gauss\n" %(src.type, src.amp, src.f0)) #waveform of type, amplitude, and freq
+        
+        if geom: #if we have a geometry file, include command 
+            f.write("#geometry_objects_read: 0 0 0 %s.h5 %s.txt\n" % (model.name+"_geometry",model.name+"_geometry"))
+
+        f.write("#hertzian_dipole: z %f %f 0 my_gauss\n" %(tx[0][i], tx[1][i])) #create tx at correct location 
+        #place rx at correct locations
+        for j in range(model.nrx):
+            f.write("#rx: %f %f %f\n" %(rx[0][j], rx[1][j],0))
+            
+        #snapshots - uses python inside of the gpr command, look at example *.in file and gprMax docs
+        f.write("#python:\nfrom gprMax.input_cmd_funcs import * \n")
+        f.write("import numpy as np\ndt = 5e-11 \nI = np.floor(float(%e*0.1/dt))\n" % src.t0)
+        f.write("N = np.floor(float(%e/dt))\nfor i in range(1,int(N)):\n"% src.t0)
+        f.write("\tsnapshot(%f,%f,0,%f,%f,%f,%f,%f,%f,i*dt,\'snapshot\'+str(i))\n#end_python:\n"%(
+            (model.x_size - model.image_domain.x_size)/2,
+            (model.y_size - model.image_domain.y_size)/2,
+            (model.x_size + model.image_domain.x_size)/2,
+            (model.y_size + model.image_domain.y_size)/2,
+            model.dx,
+            model.dx,
+            model.dx,
+            model.dx
+        ))
+
+        # extent of geometry view
+        f.write("#geometry_view: 0 0 0 %f %f %f %f %f %f %s n\n"%(model.x_size,model.y_size,model.dx,model.dy,model.dx,model.dx,model.name + '_Tx' + str(i)))
+        # output directory (only *.out receiver data)
+        f.write("#output_dir: %s" % (model.name + "_output"))
--- tests/util/test_sim.py
+++ tests/util/test_sim.py
@@ -1,7 +1,10 @@
 import unittest
 import mwi.util.sim as sim
 import mwi.util.read_config as read_config
+import mwi.util.model as model
+from mwi.util.rx import MeasurementSurface
 import os
+
 
 class TestSource(unittest.TestCase):
     example_file = "example/measurement_config.json"
@@ -16,5 +19,34 @@ class TestSource(unittest.TestCase):
         self.assertAlmostEqual(src.f0, 1e9)
         self.assertTrue(src.type == 'gaussiandot')
         self.assertAlmostEqual(src.amp, 1)
+
+class TestSim(unittest.TestCase):
+    example_file = "example/measurement_config.json"
+    model_file = "example/model_config.json"
+    domain_file = "example/image_domain.json"
+
+    rx = MeasurementSurface(read_config.read_meas_config(example_file)["measurement_surface"])
+    domain = model.ImageDomain(read_config.read_domain_config(domain_file))
+    src = sim.Source(read_config.read_meas_config(example_file)["signal"])
+
+    def test_file_exists(self):
+        # test that file exists
+        self.assertTrue(os.path.exists(self.example_file))
+        self.assertTrue(os.path.exists(self.model_file))
+        self.assertTrue(os.path.exists(self.domain_file))
+
+    def test_make_sim(self):
+        obj_model = model.Model(read_config.read_model_config(self.model_file), self.rx, self.domain)
+        sim.make(obj_model, self.src, 'example' )
+
+        with open('example/example_model_Tx0.in', 'r') as file:
+            prev = file.read()
+
+        with open('example/example_model/example_model_Tx0.in', 'r') as file:
+            new = file.read()
+        
+        self.assertTrue(prev == new)
+
+
 
         
