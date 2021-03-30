65887a93f58a79bec80cc08889e7bce269cc9b9e
Brendon Besler <brendon.besler@ucalgary.ca>
2021-03-30 15:52:58 -0600

    Update inversion

--- mwi/cli/inverse.py
+++ mwi/cli/inverse.py
@@ -11,15 +11,15 @@ import mwi.util.read_gprMax as read_gprMax
 import mwi.util.calc as calc
 
 
-def inverse(model_config_file, prior_config_file, meas_config_file, output_folder, image_config_file, born_method):
 
+def inverse(model_config_file, prior_config_file, meas_config_file, output_folder, image_config_file, born_method):
     threshold = 0.001
 
     if not(born_method == 'born') and not(born_method == 'iterative') and not(born_method == 'distorted'):
         os.sys.exit('ERROR: method {} is not supported'.format(born_method))
 
     if born_method == 'born':
-        niteration = 1
+        niteration = 10
         born_method = ''
     else:
         niteration = 10
@@ -39,8 +39,6 @@ def inverse(model_config_file, prior_config_file, meas_config_file, output_folde
     
     # initialize models (for "measured scattered fields")
     obj_model = model.Model(model_data, rx, image_domain)
-    obj_model.plot_er()
-    obj_model.plot_sig()
     bkgrd_model = model.Model(prior_data, rx, image_domain)
     
     # initialize source, make and run simulation
@@ -53,69 +51,16 @@ def inverse(model_config_file, prior_config_file, meas_config_file, output_folde
     sim.run(os.path.join(os.path.abspath(output_folder), bkgrd_model.name))
 
     # read in "measured" data
-    (total_data, _, total_data_f, _) = read_gprMax.read_out_data(os.path.join(output_folder, obj_model.name, obj_model.name + "_output"))
+    (_, _, total_data_f, _) = read_gprMax.read_out_data(os.path.join(output_folder, obj_model.name, obj_model.name + "_output"))
     (_, _, inc_data_f, freq) = read_gprMax.read_out_data(os.path.join(output_folder, bkgrd_model.name, bkgrd_model.name + "_output"))
 
     # calculate scattered fields and select measurements to use
     s_data_f = total_data_f - inc_data_f
-
-    (_, f_indx) = calc.find_nearest(freq, bkgrd_model.image_domain.freq)
-    # plt.plot(np.squeeze(np.abs(s_data_f[0, :, f_indx])))
-    # plt.plot(np.squeeze(np.roll(np.abs(s_data_f[4, :, f_indx]), 4)))
-    # plt.show()
-    # plt.plot(np.squeeze(np.abs(s_data_f[0, :, f_indx] - np.roll(s_data_f[4, :, f_indx], 4))))
-    # plt.show()
-    # plt.plot(np.squeeze(np.abs(s_data_f[1, :, f_indx])))
-    # plt.plot(np.squeeze(np.roll(np.abs(s_data_f[5, :, f_indx]), 4)))
-    # plt.show()
-    # plt.plot(np.squeeze(np.abs(s_data_f[1, :, f_indx] - np.roll(s_data_f[5, :, f_indx], 4))))
-    # plt.show()
-    # plt.plot(np.squeeze(np.abs(s_data_f[2, :, f_indx])))
-    # plt.plot(np.squeeze(np.roll(np.abs(s_data_f[6, :, f_indx]), 4)))
-    # plt.show()
-    # plt.plot(np.squeeze(np.abs(s_data_f[2, :, f_indx] - np.roll(s_data_f[6, :, f_indx], 4))))
-    # plt.show()
-    # plt.plot(np.squeeze(np.abs(s_data_f[3, :, f_indx])))
-    # plt.plot(np.squeeze(np.roll(np.abs(s_data_f[7, :, f_indx]), 4)))
-    # plt.show()
-    # plt.plot(np.squeeze(np.abs(s_data_f[3, :, f_indx] - np.roll(s_data_f[7, :, f_indx], 4))))
-    # plt.show()
-
     s_data_f = calc.select_data(bkgrd_model, s_data_f, freq)
-
-    (_,_,field_f, field_freq) = read_gprMax.read_snapshots(os.path.join(output_folder, obj_model.name))
-
-    (_,f_indx) = calc.find_nearest(field_freq, obj_model.freq)
-    
-    plt.imshow(np.abs(np.squeeze(field_f[0,f_indx,:,:])))
-    plt.show()
-    plt.imshow(np.abs(np.squeeze(field_f[4,f_indx,:,:])))
-    plt.show()
-
-    plt.imshow((np.abs(np.squeeze(field_f[0,f_indx,:,:]) - np.rot90(np.rot90(np.squeeze(field_f[4,f_indx,:,:]))))))
-    plt.show()
-    plt.imshow((np.abs(np.squeeze(field_f[1,f_indx,:,:]) - np.rot90(np.rot90(np.squeeze(field_f[5,f_indx,:,:]))))))
-    plt.show()
-    plt.imshow((np.abs(np.squeeze(field_f[2,f_indx,:,:]) - np.rot90(np.rot90(np.squeeze(field_f[6,f_indx,:,:]))))))
-    plt.show()
-    plt.imshow((np.abs(np.squeeze(field_f[3,f_indx,:,:]) - np.rot90(np.rot90(np.squeeze(field_f[7,f_indx,:,:]))))))
-    plt.show()
-
-    plt.plot(np.squeeze(total_data[0,4,:]))
-    plt.show()
-
-    
+        
     # calculate greens function
     hank_int = calc.hankel_integral(bkgrd_model)
 
-    # plt.imshow((np.abs(np.squeeze(hank_int[0,:,:,:]) - np.rot90(np.rot90(np.squeeze(hank_int[4,0,:,:]))))))
-    # plt.show()
-    # plt.imshow((np.abs(np.squeeze(hank_int[1,:,:,:]) - np.rot90(np.rot90(np.squeeze(hank_int[5,0,:,:]))))))
-    # plt.show()
-    # plt.imshow((np.abs(np.squeeze(hank_int[2,:,:,:]) - np.rot90(np.rot90(np.squeeze(hank_int[6,0,:,:]))))))
-    # plt.show()
-    # plt.imshow((np.abs(np.squeeze(hank_int[3,:,:,:]) - np.rot90(np.rot90(np.squeeze(hank_int[7,0,:,:]))))))
-    # plt.show()
     iter_model = bkgrd_model
     i = 0
     while True:
@@ -124,44 +69,48 @@ def inverse(model_config_file, prior_config_file, meas_config_file, output_folde
         
         iter_model.name = 'iteration' + str(i)
         iter_model.plot_er()
+        iter_model.plot_sig()
+
+
         sim.make(iter_model, src, output_folder)
         sim.run(os.path.join(os.path.abspath(output_folder), iter_model.name))
 
         (fields,_,field_f, field_freq) = read_gprMax.read_snapshots(os.path.join(output_folder, iter_model.name))
+        (total_data, _, total_data_f, _) = read_gprMax.read_out_data(os.path.join(output_folder, iter_model.name, iter_model.name + "_output"))
+
+        iter_rx_data = total_data_f - inc_data_f
+        iter_s_data_f = calc.select_data(bkgrd_model, iter_rx_data, freq)
+
+        (_, rmse) = calc.residuals_percent(s_data_f, iter_s_data_f.flatten())
+        print(f"Root mean square error: {rmse:0.2f}%")
+        plt.plot(np.abs(iter_s_data_f.flatten()), label = 'Simulated')
+        plt.plot(np.abs(s_data_f.flatten()), label = 'Measured')
+        plt.ylabel("Magnitude [ ]")
+        plt.legend()
+        plt.title("Scattered Fields at Receivers")
+        plt.show()
+        plt.plot(np.angle(iter_s_data_f.flatten()), label = 'Simulated')
+        plt.plot(np.angle(s_data_f.flatten()), label = 'Measured')
+        plt.ylabel("Phase [rad]")
+        plt.legend()
+        plt.title("Scattered Fields at Receivers")
+        plt.show()
+
 
         (_,f_indx) = calc.find_nearest(field_freq, iter_model.freq)
 
-        plt.plot(np.squeeze(fields[0,:,10,0]))
-        plt.plot(np.squeeze(fields[4,:,10,19]))
-        plt.show()
-
-        print(np.mean(np.abs(np.squeeze(field_f[0,f_indx,:,:]))))
-        plt.imshow(np.abs(np.squeeze(field_f[0,f_indx,:,:])))
-        plt.show()
-        plt.imshow(np.abs(np.squeeze(field_f[4,f_indx,:,:])))
-        plt.show()
-
-        plt.imshow((np.abs(np.squeeze(field_f[0,f_indx,:,:]) - np.rot90(np.rot90(np.squeeze(field_f[4,f_indx,:,:]))))))
-        plt.show()
-        plt.imshow((np.abs(np.squeeze(field_f[1,f_indx,:,:]) - np.rot90(np.rot90(np.squeeze(field_f[5,f_indx,:,:]))))))
-        plt.show()
-        plt.imshow((np.abs(np.squeeze(field_f[2,f_indx,:,:]) - np.rot90(np.rot90(np.squeeze(field_f[6,f_indx,:,:]))))))
-        plt.show()
-        plt.imshow((np.abs(np.squeeze(field_f[3,f_indx,:,:]) - np.rot90(np.rot90(np.squeeze(field_f[7,f_indx,:,:]))))))
-        plt.show()
-
-        plt.imshow(np.abs(np.squeeze(field_f[0,f_indx,:,:])))
-        plt.show()
-        plt.imshow(np.abs(np.squeeze(field_f[4,f_indx,:,:])))
-        plt.show()
-
+        print("Incident field shape: ")
+        print(field_f.shape)
 
         # form data operator matrix
         data_op = calc.form_data_operator(iter_model, hank_int, field_f, field_freq)
-        
+
+        plt.plot(np.abs(s_data_f))
+        plt.title("Rx scatter")
+        plt.show()
         # make L-curve and find optimum
         print("Calculating L-Curve")
-        (res_norm, soln_norm, gamma) = calc.L_curve(s_data_f, data_op, 10)
+        (res_norm, soln_norm, gamma) = calc.L_curve(s_data_f, data_op, 100)
         (kappa, opt_gamma, gamma_idx) = calc.L_curve_knee(np.log10(res_norm), np.log10(soln_norm), gamma)
 
         # plot L-curve
@@ -177,6 +126,8 @@ def inverse(model_config_file, prior_config_file, meas_config_file, output_folde
         plt.semilogx(opt_gamma, kappa[gamma_idx])
         plt.show()
 
+        np.save("rx_data.npy",s_data_f)
+
         # do inversion
         contrast = calc.solve_regularized(s_data_f, data_op, opt_gamma, np.identity((data_op.shape[1])))
 
@@ -189,29 +140,14 @@ def inverse(model_config_file, prior_config_file, meas_config_file, output_folde
 
         # form er image
         er = np.reshape(contrast.real +1, (iter_model.image_domain.y_cell.size, iter_model.image_domain.x_cell.size))
-        indx = (er < 1)
-        er[indx] = 1.0
         er_imag = np.reshape(contrast.imag, (iter_model.image_domain.y_cell.size, iter_model.image_domain.x_cell.size))
-        indx = (er_imag < 0)
-        er_imag[indx] = 0
 
-
-        plt.imshow(er - np.rot90(np.rot90(er)))
-        plt.show()
-
-        plt.imshow(er - np.rot90(er))
-        plt.show()
-
-        plt.imshow(er_imag - np.rot90(np.rot90(er_imag)))
-        plt.show()
-
-        plt.imshow(er_imag - np.rot90(er_imag))
-        plt.show()
 
         # compare er image to model
-        (rsse_x, rsse_y) = obj_model.compare_to_image(er, 'er', True)
+        (rsse_x, rsse_y, rsse_total) = obj_model.compare_to_image(er, 'er', True)
         print(f"Root of sum square error in x: {rsse_x:0.2f}")
         print(f"Root of sum square error in y: {rsse_y:0.2f}")
+        print(f"Root of sum square error over image: {rsse_total:0.2f}")
 
         # plot scattered fields
         plt.plot(np.abs(s_data_calc), label = "Calculated")
@@ -259,11 +195,14 @@ def inverse(model_config_file, prior_config_file, meas_config_file, output_folde
             break
         
         # update model
+        indx = (er_imag < 0)
+        er_imag[indx] = 0
+
+        indx = (er < 1)
+        er[indx] = 1.0
+
         iter_model.write_image(er, 'er')
         iter_model.write_image(er_imag, 'er_imag')
-        
-        plt.imshow(iter_model.get_image('er') - obj_model.get_image('er'))
-        plt.show()
 
 
     
